f65954c9b7fd45d70444fd3f347c9d4f9bb1dbda
Deepika242 <deepikasivapuram242@gmail.com>
2024-08-07 20:59:09 +0530

    added backend

---
+++ routes/pokemonRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const pokemonController = require("../controllers/pokemonController");
+
+router.get("/pokemon", pokemonController.getAllPokemons);
+router.post("/pokemon", pokemonController.addPokemon);
+router.post("/pokemon/:ownerName/add", pokemonController.addPokemonToUser);
+router.delete("/pokemon/:ownerName", pokemonController.deletePokemon);
+router.delete("/pokemon", pokemonController.deleteAllPokemons);
+router.get("/pokemon-owners", pokemonController.getAllPokemonOwners);
+router.get("/pokemon/:ownerName", pokemonController.getPokemonByOwner);
+router.put(
+  "/pokemon/:pokemonOwnerName",
+  pokemonController.getPokemonByOwnerAndUpdate
+);
+
+module.exports = router;
